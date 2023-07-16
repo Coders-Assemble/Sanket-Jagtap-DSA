6142c2dd125f7d1cfc8141da45243c590ad922f6
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-07-16 21:05:32 +0530

    File added in stack folder

--- Stacks/Stacks-14-PrevSmallerElement.cpp
+++ Stacks/Stacks-14-PrevSmallerElement.cpp
@@ -3,21 +3,26 @@
 #include <stack>
 using namespace std;
 
-vector<int> nextSmallerElement(vector<int> &input)
+vector<int> prevSmallerElement(vector<int> &input)
 {
     stack<int> s;
     s.push(-1);
 
     vector<int> ans(input.size());
-    for (int i = input.size()-1; i >= 0; i--)
+    for (int i = 0; i < input.size(); i++)
     {
         int curr = input[i];
 
+        // Pop elements from stack until you don't get element smaller than current element
         while (s.top() >= curr)
         {
             s.pop();
         }
+
+        // Found smaller element -> Store in ans vector
         ans[i] = s.top();
+
+        // Push current element in the stack
         s.push(curr);
     }
     return ans;
@@ -25,16 +30,16 @@ vector<int> nextSmallerElement(vector<int> &input)
 
 int main()
 {
-
     vector<int> input;
+
     input.push_back(2);
     input.push_back(1);
     input.push_back(4);
     input.push_back(3);
 
-    vector<int> ans = nextSmallerElement(input);
+    vector<int> ans = prevSmallerElement(input);
 
-    cout << "Printing" << endl;
+    cout << "Printing the Output vector" << endl;
     for (int i = 0; i < input.size(); i++)
     {
         cout << ans[i] << " ";
