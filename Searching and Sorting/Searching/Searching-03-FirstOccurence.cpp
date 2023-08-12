6bc10bdf19e10bab2c21956be23029f7bf726911
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-08-13 01:47:00 +0530

    FirstOcc and LastOcc filesadded

---
+++ Searching and Sorting/Searching/Searching-03-FirstOccurence.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int firstOcc(vector<int> &arr, int target)
+{
+    int start = 0;
+    int end = arr.size() - 1;
+
+    int mid = start + ((end - start) / 2);
+    int ans = -1;
+
+    while (start <= end)
+    {
+        if (arr[mid] == target)
+        {
+            // index store
+            ans = mid;
+            // left search
+            end = mid - 1;
+        }
+        else if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else if (arr[mid] > target)
+        {
+            end = mid - 1;
+        }
+        mid = start + ((end - start) / 2);
+    }
+    return ans;
+}
+
+int main()
+{
+
+    vector<int> v{1, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 7, 8, 9};
+    int target = 4;
+
+    int indexOfElement = firstOcc(v, target);
+
+    cout << "First occurence of " << target << " is at " << indexOfElement << endl;
+
+    return 0;
+}
