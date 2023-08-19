5fb3b945e656caadfc7d298ab6789c5f925606d3
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-08-20 01:10:24 +0530

    Find K Closest Elements approach-02

--- Searching and Sorting/Assignment Questions/Find K Closest Elements.cpp
+++ Searching and Sorting/Assignment Questions/Find K Closest Elements.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+// Two pointers approach
 class Solution
 {
 public:
@@ -28,5 +29,63 @@ public:
             ans.push_back(arr[i]);
         }
         return ans;
+    }
+};
+
+// Binary search approach
+
+class Solution
+{
+public:
+    int lowerBound(vector<int> &arr, int x)
+    {
+        int s = 0;
+        int e = arr.size() - 1;
+        int ans = e;
+        while (s <= e)
+        {
+            int mid = s + (e - s) / 2;
+
+            if (arr[mid] >= x)
+            {
+                ans = mid;
+                e = mid - 1;
+            }
+            else if (arr[mid] < x)
+            {
+                s = mid + 1;
+            }
+            else if (arr[mid] > x)
+            {
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> findClosestElements(vector<int> &arr, int k, int x)
+    {
+        int h = lowerBound(arr, x);
+        int l = h - 1;
+        while (k--)
+        {
+            if (l < 0)
+            {
+                h++;
+            }
+            else if (h >= arr.size())
+            {
+                l--;
+            }
+            else if (x - arr[l] > arr[h] - x)
+            {
+                h++;
+            }
+            else
+            {
+                l--;
+            }
+        }
+        return vector<int>(arr.begin() + l + 1, arr.begin() + h);
     }
 };
