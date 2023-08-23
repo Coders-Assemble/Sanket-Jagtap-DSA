b120e7f7aa4d73f0dceb155b851812437f1fe6b4
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-08-23 11:11:04 +0530

    K-diff Pairs in an Array - Binary search approach

--- Searching and Sorting/Assignment Questions/K-diff Pairs in an Array.cpp
+++ Searching and Sorting/Assignment Questions/K-diff Pairs in an Array.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 using namespace std;
 
+// Two pointers approach
 class Solution
 {
 public:
@@ -34,6 +35,51 @@ public:
             if (i == j)
                 j++;
         }
+        return ans.size();
+    }
+};
+
+// Binary search approach
+class Solution
+{
+public:
+    int binarySearch(vector<int> &nums, int start, int x)
+    {
+        int end = nums.size() - 1;
+
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == x)
+            {
+                return mid;
+            }
+            else if (x > nums[mid])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    int findPairs(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+        set<pair<int, int>> ans;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (binarySearch(nums, i + 1, nums[i] + k) != -1)
+            {
+                ans.insert({nums[i], nums[i] + k});
+            }
+        }
+
         return ans.size();
     }
 };
