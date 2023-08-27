0a14cfacfafcaa7ca1041c10dc1d9683090bdd5e
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-08-27 19:51:53 +0530

    Find Peak Element

---
+++ Searching and Sorting/Searching/Searching-15-FindPeakElement.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 1)
+            return 0;
+
+        if (nums[0] > nums[1])
+            return 0;
+
+        if (nums[n - 1] > nums[n - 2])
+            return n - 1;
+
+        int start = 1;
+        int end = n - 2;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
+            {
+                return mid;
+            }
+            else if (nums[mid] > nums[mid - 1])
+            {
+                start = mid + 1;
+            }
+            else if (nums[mid] > nums[mid + 1])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
+
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        int start = 0;
+        int end = nums.size() - 1;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (start == end)
+                return start;
+            if (nums[mid] > nums[mid + 1])
+            {
+                end = mid;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
