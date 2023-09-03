55a3938f6664afef038e1bcf6f20bb872391e9ff
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-03 19:51:14 +0530

    Search in rotated sorted array - Approach-3

--- Searching and Sorting/Searching/Searching-10-SearchInRotatedSortedArray.cpp
+++ Searching and Sorting/Searching/Searching-10-SearchInRotatedSortedArray.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+// Approach 01
 class Solution
 {
 public:
@@ -84,6 +85,7 @@ public:
     }
 };
 
+// Approach 02
 class Solution
 {
 public:
@@ -120,6 +122,39 @@ public:
                 {
                     end = mid - 1;
                 }
+            }
+        }
+        return -1;
+    }
+};
+
+// Approach 03
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int low = 0, high = nums.size() - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[low] <= nums[mid])
+            {
+                if (nums[low] <= target && target <= nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            else
+            {
+                if (nums[mid] <= target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
             }
         }
         return -1;
