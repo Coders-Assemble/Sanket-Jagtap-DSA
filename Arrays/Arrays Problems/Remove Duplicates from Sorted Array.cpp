ee9ec8093336334eb89cd1ddc553d14be902780b
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-14 18:27:35 +0530

    Remove Duplicates from Sorted Array-Brute force approach

--- Arrays/Arrays Problems/Remove Duplicates from Sorted Array.cpp
+++ Arrays/Arrays Problems/Remove Duplicates from Sorted Array.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
 
+// Brute force approach
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        set<int> st;
+        for (int i = 0; i < nums.size(); i++)
+            st.insert(nums[i]);
+
+        int index = 0;
+        for (auto it : st)
+        {
+            nums[index] = it;
+            index++;
+        }
+        return index;
+    }
+};
+
+// Two pointers approach
 class Solution
 {
 public:
