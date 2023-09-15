555408deeeb918ec30fd0e399504e6b8ac17a603
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-15 21:17:20 +0530

    Rotate array to the left

--- Arrays/Arrays Problems/Rotate Array.cpp
+++ Arrays/Arrays Problems/Rotate Array.cpp
@@ -16,3 +16,38 @@ vector<int> rotateArray(vector<int> &arr, int n)
 
     return arr;
 }
+
+// Left Rotate - Brute force
+// vector<int> rotateArray(vector<int> arr, int k)
+// {
+//     int n = arr.size();
+//     int temp[k];
+//     for (int i = 0; i < k; i++)
+//     {
+//         temp[i] = arr[i];
+//     }
+
+//     for (int i = k; i < n; i++)
+//     {
+//         arr[i - k] = arr[i];
+//     }
+
+//     int j = 0;
+//     for (int i = n - k; i < n; i++)
+//     {
+//         arr[i] = temp[j];
+//         j++;
+//     }
+//     return arr;
+// }
+
+// Left Rotate - Optimal
+vector<int> rotateArray(vector<int> arr, int k)
+{
+    int n = arr.size();
+    reverse(arr.begin(), arr.begin() + k);
+    reverse(arr.begin() + k, arr.begin() + n);
+    reverse(arr.begin(), arr.begin() + n);
+
+    return arr;
+}
