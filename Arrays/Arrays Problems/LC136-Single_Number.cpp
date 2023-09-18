3dc278701a5039d4291bc003346e6ba843bec716
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-18 21:02:47 +0530

    Single Number-Optimal approach

--- Arrays/Arrays Problems/LC136-Single_Number.cpp
+++ Arrays/Arrays Problems/LC136-Single_Number.cpp
@@ -25,3 +25,17 @@ public:
     }
 };
 
+// Optimal approach
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+};
