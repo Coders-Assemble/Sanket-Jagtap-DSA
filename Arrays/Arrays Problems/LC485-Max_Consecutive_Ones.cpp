18283be63fa83e0ecd5e08a889623b4269ac05e0
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-18 20:26:59 +0530

    Max Consecutive Ones

---
+++ Arrays/Arrays Problems/LC485-Max_Consecutive_Ones.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int maxCount = 0;
+        int currentCount = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 1)
+            {
+                currentCount++;
+                maxCount = max(maxCount, currentCount);
+            }
+            else
+            {
+                currentCount = 0;
+            }
+        }
+        return maxCount;
+    }
+};
