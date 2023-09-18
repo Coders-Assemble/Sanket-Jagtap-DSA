9bec143a9eb7f83a1df1f7983691d733ca21a259
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-18 12:17:05 +0530

    Missing Number-Better Approach

--- Arrays/Arrays Problems/LC_268_Missing_Number.cpp
+++ Arrays/Arrays Problems/LC_268_Missing_Number.cpp
@@ -31,6 +31,25 @@ public:
     }
 };
 
+// Better approach
+int missingNumber(vector<int> &nums)
+{
+    int N = nums.size();
+    vector<int> hash(N + 1, 0);
+
+    for (int i = 0; i < N; i++)
+        hash[nums[i]]++;
+
+    for (int i = 0; i <= N; i++)
+    {
+        if (hash[i] == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 // Optimal approach-1
 class Solution
 {
