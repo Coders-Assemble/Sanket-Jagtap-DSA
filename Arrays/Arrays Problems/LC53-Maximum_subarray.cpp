bed6f0fafb2c8cb5e9c61fb6074ae75071b39057
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-23 19:36:27 +0530

    Maximum subarray - Kadane's Algorithm

--- Arrays/Arrays Problems/LC53-Maximum_subarray.cpp
+++ Arrays/Arrays Problems/LC53-Maximum_subarray.cpp
@@ -27,3 +27,29 @@ public:
     }
 };
 
+// Optimal approach - Kadane's Algorithm
+// TC => O(n)
+// SC => O(1)
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int sum = 0;
+        int maxi = nums[0];
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum = sum + nums[i];
+
+            maxi = max(maxi, sum);
+
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+};
