97aa07cebbab5dd281518f1612aa8517593f09bd
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-23 21:07:33 +0530

    Stock buy and sell - Optimal approach

--- Arrays/Arrays Problems/LC121-Stock_buy_and_sell.cpp
+++ Arrays/Arrays Problems/LC121-Stock_buy_and_sell.cpp
@@ -22,3 +22,27 @@ int bestTimeToBuyAndSellStock(vector<int> &prices)
     }
     return maxProfit;
 }
+
+// Optimal approach
+// TC => O(n)
+// SC => O(1)
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int mini = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.size(); i++)
+        {
+            int profit = prices[i] - mini;
+
+            maxProfit = max(maxProfit, profit);
+
+            mini = min(mini, prices[i]);
+        }
+        return maxProfit;
+    }
+};
