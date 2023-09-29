615b5a0ec022adb42c384cc9774ae2b1129af301
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-29 13:29:15 +0530

    Longest Subarray With Sum K - Using Map

--- Arrays/Arrays Problems/Longest_Subarray_With_Sum_K.cpp
+++ Arrays/Arrays Problems/Longest_Subarray_With_Sum_K.cpp
@@ -29,3 +29,35 @@ int longestSubarrayWithSumK(vector<int> arr, long long k)
     return maxLen;
 }
 
+// Better Approach - Using Map
+// TC => O(nlogn)
+// SC => O(n)
+
+int getLongestSubarray(vector<int> &a, int k)
+{
+    map<long long, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        sum += a[i];
+
+        if (sum == k)
+        {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        int rem = sum - k;
+        if (preSumMap.find(rem) != preSumMap.end())
+        {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        if (preSumMap.find(sum) == preSumMap.end())
+        {
+            preSumMap[sum] = i;
+        }
+    }
+    return maxLen;
+}
