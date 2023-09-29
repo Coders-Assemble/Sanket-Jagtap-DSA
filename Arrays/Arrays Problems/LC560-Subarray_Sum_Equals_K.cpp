487d43024fbe98e03cd1e0e52bc8ed81b7f695d9
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-09-29 18:12:40 +0530

    Subarray Sum equals K - Using Hashmap

--- Arrays/Arrays Problems/LC560-Subarray_Sum_Equals_K.cpp
+++ Arrays/Arrays Problems/LC560-Subarray_Sum_Equals_K.cpp
@@ -31,3 +31,31 @@ public:
     }
 };
 
+// Optimal Approach
+// TC => O(N) or O(NlogN)
+// Here we are using unorderd map in which for best and avg case map opertions takes O(1) and worst case takes O(N).
+// If we use Map, map operations takes O(logN).
+// SC => O(N) as we are using a map data structure.
+
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mpp;
+        mpp[0] = 1;
+        int count = 0;
+        int sum = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+
+            int remove = sum - k;
+            count += mpp[remove];
+            mpp[sum]++;
+        }
+        return count;
+    }
+};
