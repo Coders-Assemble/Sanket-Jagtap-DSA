1d674409ced586e949e9116a565e810b4df90ec4
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-10-04 13:35:33 +0530

    Print the entire Pascal's Triangle

--- Arrays/Arrays Problems/LC118-Pascal's_Triangle.cpp
+++ Arrays/Arrays Problems/LC118-Pascal's_Triangle.cpp
@@ -34,20 +34,58 @@ int main()
 // TC => O(N)
 // SC => O(1)
 
-void ansRow(int n){
+void ansRow(int n)
+{
     long long ans = 1;
-    cout<<ans<<" ";
-    for(int i=1; i<n; i++){
-        ans = ans * (n-i);
+    cout << ans << " ";
+    for (int i = 1; i < n; i++)
+    {
+        ans = ans * (n - i);
         ans = ans / i;
-        cout<<ans<<" ";
+        cout << ans << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
-int main(){
+int main()
+{
 
     ansRow(5);
 
     return 0;
 }
+
+// Type - 3
+// Given N, Print the entire Pascal's Triangle
+// TC => O(N^2)
+// SC => O(1)
+
+class Solution
+{
+public:
+    vector<int> generateRows(int n)
+    {
+        long long ans = 1;
+        vector<int> ansRow;
+        ansRow.push_back(1);
+
+        for (int i = 1; i < n; i++)
+        {
+            ans = ans * (n - i);
+            ans = ans / i;
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
+
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> ans;
+
+        for (int i = 1; i <= numRows; i++)
+        {
+            ans.push_back(generateRows(i));
+        }
+        return ans;
+    }
+};
