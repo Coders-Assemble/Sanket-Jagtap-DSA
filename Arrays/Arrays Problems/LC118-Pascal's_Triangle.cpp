3cd8de1273765de6fbe6b6fd83a59f84b2c2fca2
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-10-04 13:32:36 +0530

    Print Nth row of Pascal's Triangle

--- Arrays/Arrays Problems/LC118-Pascal's_Triangle.cpp
+++ Arrays/Arrays Problems/LC118-Pascal's_Triangle.cpp
@@ -2,13 +2,17 @@
 #include <vector>
 using namespace std;
 
+// // Type - 1
 // Given row and column, Find the element at that place
-int findElementInPascalTriangle(int r, int c)
+// TC => O(r)
+// SC => O(1)
+
+int findElementInPascalTriangle(int n, int r)
 {
     int res = 1;
-    for (int i = 0; i < c; i++)
+    for (int i = 0; i < r; i++)
     {
-        res = res * (r - i);
+        res = res * (n - i);
         res = res / (i + 1);
     }
     return res;
@@ -21,6 +25,29 @@ int main()
 
     int ans = findElementInPascalTriangle(r - 1, c - 1);
     cout << "Element at " << r << "," << c << " is " << ans << endl;
+
+    return 0;
+}
+
+// Type - 2
+// Print any Nth row of Pascal's Triangle
+// TC => O(N)
+// SC => O(1)
+
+void ansRow(int n){
+    long long ans = 1;
+    cout<<ans<<" ";
+    for(int i=1; i<n; i++){
+        ans = ans * (n-i);
+        ans = ans / i;
+        cout<<ans<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+
+    ansRow(5);
 
     return 0;
 }
