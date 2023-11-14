1dfaccb4464cc3dbb9ad8b0676ca6b44256b7763
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-11-14 15:52:13 +0530

    Move All Negative Numbers To Left

---
+++ Arrays/Arrays Problems/Move_All_Negative_Numbers_To_Left.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void moveAllNegToLeft(vector<int> &v)
+{
+    int low = 0;
+    int high = v.size() - 1;
+
+    while (low < high)
+    {
+        if (v[low] < 0)
+        {
+            low++;
+        }
+        else if (v[high] > 0)
+        {
+            high--;
+        }
+        else
+        {
+            swap(v[low], v[high]);
+        }
+    }
+}
+
+int main()
+{
+
+    // vector<int> v{-1, 2, 3, -4, 5, -6, 7};
+    vector<int> v{1, 0, -1, -2, -3, -4, -5, -6, -7};
+
+    moveAllNegToLeft(v);
+
+    for (auto it : v)
+    {
+        cout << it << " ";
+    }
+
+    return 0;
+}
