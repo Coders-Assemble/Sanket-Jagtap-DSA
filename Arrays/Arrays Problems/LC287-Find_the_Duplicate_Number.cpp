00769a93b8da388fc68ef58492c506cfa00d923f
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-11-14 17:29:28 +0530

    Find the Duplicate Number

---
+++ Arrays/Arrays Problems/LC287-Find_the_Duplicate_Number.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Time Limit Exceeded
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    return nums[i];
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+// Works but modifies the array
+// TC => O(nlogn)
+// SC => O(n)
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
+
+// Works but modifies the array
+// TC => O(n)
+// SC => O(1)
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int index = abs(nums[i]);
+
+            if (nums[index] < 0)
+            {
+                return index;
+            }
+
+            nums[index] *= -1;
+        }
+        return -1;
+    }
+};
+
+// Works but modifies the array
+// TC => O(n)
+// SC => O(1)
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        while (nums[0] != nums[nums[0]])
+        {
+            swap(nums[0], nums[nums[0]]);
+        }
+        return nums[0];
+    }
+};
