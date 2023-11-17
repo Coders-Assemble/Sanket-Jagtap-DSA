22417193d7e9666751fab4dfbef694c0d3756259
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-11-17 22:54:28 +0530

    Contains Duplicate

--- Arrays/Arrays Problems/LC217-Contains_Duplicate.cpp
+++ Arrays/Arrays Problems/LC217-Contains_Duplicate.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 // Using HashMap
@@ -25,6 +26,30 @@ public:
                 return true;
             }
         }
+        return false;
+    }
+};
+
+// Using set
+// TC => O(n)
+// SC => O(n)
+
+class Solution
+{
+public:
+    bool containsDuplicate(vector<int> &nums)
+    {
+        unordered_set<int> s;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (s.find(nums[i]) != s.end())
+            {
+                return true;
+            }
+            s.insert(nums[i]);
+        }
+
         return false;
     }
 };
