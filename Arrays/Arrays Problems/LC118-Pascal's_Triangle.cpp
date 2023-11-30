0965eee6b6bb98bd1ba83419f568271a6ff618b7
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-11-30 20:01:25 +0530

    Pascal's triangle updated

--- Arrays/Arrays Problems/LC118-Pascal's_Triangle.cpp
+++ Arrays/Arrays Problems/LC118-Pascal's_Triangle.cpp
@@ -89,3 +89,31 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> result;
+        vector<int> v;
+
+        for (int row = 1; row <= numRows; row++)
+        {
+            int ans = 1;
+            v.push_back(1);
+
+            for (int col = 1; col < row; col++)
+            {
+                ans = ans * (row - col);
+                ans = ans / col;
+                v.push_back(ans);
+            }
+
+            result.push_back(v);
+            v.clear();
+        }
+
+        return result;
+    }
+};
