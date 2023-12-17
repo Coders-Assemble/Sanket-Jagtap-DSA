236db5f92888523d821cadbb000bed082bc46d4e
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-12-17 19:34:12 +0530

    Longest common prefix updated

--- Char and Strings/Strings Problems/LC14-Longest_Common_Prefix.cpp
+++ Char and Strings/Strings Problems/LC14-Longest_Common_Prefix.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 // (TC) => O(S), where S is the sum of the lengths of all the input strings.
@@ -49,5 +50,64 @@ public:
             i++;
         }
         return ans;
+    }
+};
+
+// TC => O(n*m) where 'n' is the number of strings in the vector and 'm' is the length of the common prefix
+// SC => O(1)
+
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.size(); i++)
+        {
+            int j = 0;
+            while (j < prefix.size() && j < strs[i].size() && prefix[j] == strs[i][j])
+            {
+                j++;
+            }
+
+            prefix = prefix.substr(0, j);
+
+            if (prefix.empty())
+            {
+                return "";
+            }
+        }
+        return prefix;
+    }
+};
+
+// TC => O(n * log(n) + m)
+// SC => O(1)
+
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        int n = strs.size();
+
+        sort(strs.begin(), strs.end());
+        string a = strs[0];
+        string b = strs[n - 1];
+
+        int i = 0;
+        while (i < a.size())
+        {
+            if (a[i] == b[i])
+            {
+                i++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return a.substr(0, i);
     }
 };
