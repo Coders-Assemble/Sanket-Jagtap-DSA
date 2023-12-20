236eb2bddac65450c3ec299d780d0a2f6042ffc1
Sanket Jagtap <sanketjagtap4622@gmail.com>
2023-12-21 00:13:53 +0530

    Largest Number

---
+++ Arrays/Arrays Problems/LC179-Largest_Number.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+// TC => O(nlogn)
+// SC => O(n)
+
+class Solution
+{
+public:
+    static bool mysort(string a, string b)
+    {
+        return a + b > b + a;
+    }
+
+    string largestNumber(vector<int> &nums)
+    {
+        string s = "";
+        vector<string> all_numbers;
+
+        for (int it : nums)
+        {
+            all_numbers.push_back(to_string(it));
+        }
+
+        sort(all_numbers.begin(), all_numbers.end(), mysort);
+        if (all_numbers[0] == "0")
+        {
+            return "0";
+        }
+
+        for (string a : all_numbers)
+        {
+            s += a;
+        }
+
+        return s;
+    }
+};
