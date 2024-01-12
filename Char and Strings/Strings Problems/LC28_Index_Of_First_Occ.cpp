3af118099083dadaf05dd221e7ec0a95da352fff
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-01-12 22:20:35 +0530

    Index of First Occurence using KMP Algorithm

--- Char and Strings/Strings Problems/LC28_Index_Of_First_Occ.cpp
+++ Char and Strings/Strings Problems/LC28_Index_Of_First_Occ.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <string>
 using namespace std;
 
@@ -49,6 +50,73 @@ public:
                     return i;
                 }
             }
+        }
+        return -1;
+    }
+};
+
+class Solution
+{
+public:
+    void lpsFind(vector<int> &lps, string needle)
+    {
+        int prefix = 0;
+        int suffix = 1;
+
+        while (suffix < needle.size())
+        {
+            // Matched
+            if (needle[prefix] == needle[suffix])
+            {
+                lps[suffix] = prefix + 1;
+                prefix++, suffix++;
+            }
+            // Unmathed
+            else
+            {
+                if (prefix == 0)
+                {
+                    lps[suffix] = 0;
+                    suffix++;
+                }
+                else
+                {
+                    prefix = lps[prefix - 1];
+                }
+            }
+        }
+    }
+
+    int strStr(string haystack, string needle)
+    {
+        vector<int> lps(needle.size(), 0);
+        lpsFind(lps, needle);
+        int first = 0, second = 0;
+
+        while (first < haystack.size() && second < needle.size())
+        {
+            // Matched
+            if (haystack[first] == needle[second])
+            {
+                first++, second++;
+            }
+            // Unmatched
+            else
+            {
+                if (second == 0)
+                {
+                    first++;
+                }
+                else
+                {
+                    second = lps[second - 1];
+                }
+            }
+        }
+
+        if (second == needle.size())
+        {
+            return first - second;
         }
         return -1;
     }
