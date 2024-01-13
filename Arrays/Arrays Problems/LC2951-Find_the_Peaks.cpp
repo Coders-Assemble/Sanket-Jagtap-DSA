15e219d70e37eead9b8b4102c7af0fcc31499afb
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-01-13 20:40:12 +0530

    Find the Peaks

---
+++ Arrays/Arrays Problems/LC2951-Find_the_Peaks.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// TC => O(n)
+// SC => O(n)
+
+class Solution
+{
+public:
+    vector<int> findPeaks(vector<int> &mountain)
+    {
+        vector<int> vec;
+        for (int i = 1; i < mountain.size() - 1; i++)
+        {
+            if (mountain[i - 1] < mountain[i] && mountain[i] > mountain[i + 1])
+            {
+                vec.push_back(i);
+            }
+        }
+        return vec;
+    }
+};
