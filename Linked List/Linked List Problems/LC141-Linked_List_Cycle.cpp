b82296eabbf1b4745c74cc20524a85a57812baf9
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-01-20 23:44:01 +0530

    Linked List Cycle

---
+++ Linked List/Linked List Problems/LC141-Linked_List_Cycle.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// TC => O(n)
+// SC => O(1)
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (head == NULL)
+            return false;
+
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
