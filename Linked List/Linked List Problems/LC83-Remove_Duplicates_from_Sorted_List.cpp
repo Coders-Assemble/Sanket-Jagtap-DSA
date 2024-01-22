5a5594bee327da9d3e4a7e83bebf80005e81eb06
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-01-23 00:13:02 +0530

    Remove Duplicates From Sorted List

---
+++ Linked List/Linked List Problems/LC83-Remove_Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// TC => O(n)
+// SC => O(1)
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+
+        ListNode *curr = head;
+
+        while (curr != NULL && curr->next != NULL)
+        {
+            if (curr->val == curr->next->val)
+            {
+                ListNode *temp = curr->next;
+                curr->next = temp->next;
+                temp->next = NULL;
+                delete temp;
+            }
+            else
+            {
+                curr = curr->next;
+            }
+        }
+
+        return head;
+    }
+};
