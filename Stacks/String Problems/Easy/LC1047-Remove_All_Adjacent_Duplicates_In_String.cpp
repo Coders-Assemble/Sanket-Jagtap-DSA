9496710462beb1897b7c4fa29df5dfa34e0a031d
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-01-26 21:12:15 +0530

    Remove All Adjacent Duplicates In String

---
+++ Stacks/String Problems/Easy/LC1047-Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// TC => O(n)
+// SC => O(n)
+
+class Solution
+{
+public:
+    string removeDuplicates(string s)
+    {
+        stack<char> st;
+
+        for (auto ch : s)
+        {
+            if (!st.empty() && st.top() == ch)
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(ch);
+            }
+        }
+
+        string ans;
+        while (!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
