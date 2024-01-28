508d0e56ad2f37956e22d1b01fb50be4ebcbdf8e
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-01-29 01:45:03 +0530

    Minimum Add to Make Parantheses Valid

---
+++ Char and Strings/Strings Problems/LC921-Minimum_Add_to_Make_Parentheses_Valid.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// TC => O(n)
+// SC => O(1)
+
+class Solution
+{
+public:
+    int minAddToMakeValid(string s)
+    {
+        int open = 0;
+        int close = 0;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(')
+            {
+                open++;
+            }
+            else if (s[i] == ')' && open > 0)
+            {
+                open--;
+            }
+            else
+            {
+                close++;
+            }
+        }
+
+        return open + close;
+    }
+};
