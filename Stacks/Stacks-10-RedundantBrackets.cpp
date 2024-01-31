583709fa94e02f9824722794db9f3f6db88b4169
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-01-31 22:36:24 +0530

    Redundant brackets

--- Stacks/Stacks-10-RedundantBrackets.cpp
+++ Stacks/Stacks-10-RedundantBrackets.cpp
@@ -2,7 +2,33 @@
 #include <stack>
 using namespace std;
 
-int main(){
-
-    return 0;
-}
+class Solution
+{
+public:
+    int checkRedundancy(string s)
+    {
+        stack<char> st;
+        for (auto ch : s)
+        {
+            if (ch == '(' || ch == '+' || ch == '-' || ch == '*' || ch == '/')
+            {
+                st.push(ch);
+            }
+            else if (ch == ')')
+            {
+                if (st.top() == '(')
+                {
+                    return 1;
+                }
+                else
+                {
+                    while (st.top() == '+' || st.top() == '-' || st.top() == '*' || st.top() == '/')
+                        st.pop();
+                    if (st.top() == '(')
+                        st.pop();
+                }
+            }
+        }
+        return 0;
+    }
+};
