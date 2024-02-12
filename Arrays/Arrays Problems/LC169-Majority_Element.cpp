dafa2147ef8d59dd4e16c013c0db6dacbc5021d1
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-02-13 00:14:14 +0530

    Majority Element - Moore's Voting Algorithm

--- Arrays/Arrays Problems/LC169-Majority_Element.cpp
+++ Arrays/Arrays Problems/LC169-Majority_Element.cpp
@@ -55,3 +55,36 @@ int majorityElement(vector<int> nums)
     }
     return -1;
 }
+
+// Optimal Approach - Moore's Voting Algorithm
+// TC => O(n)
+// SC => O(1)
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        int cnt = 0;
+        int el;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (cnt == 0)
+            {
+                cnt = 1;
+                el = nums[i];
+            }
+            else if (nums[i] == el)
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
+        }
+        return el;
+    }
+};
