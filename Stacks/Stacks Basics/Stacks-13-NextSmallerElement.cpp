fc8f501f4acdb6f7fc689cd8c403d940fab1b71d
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-02-15 21:32:34 +0530

    Next Smaller Element

--- Stacks/Stacks Basics/Stacks-13-NextSmallerElement.cpp
+++ Stacks/Stacks Basics/Stacks-13-NextSmallerElement.cpp
@@ -9,7 +9,7 @@ vector<int> nextSmallerElement(vector<int> &input)
     s.push(-1);
 
     vector<int> ans(input.size());
-    for (int i = input.size()-1; i >= 0; i--)
+    for (int i = input.size() - 1; i >= 0; i--)
     {
         int curr = input[i];
 
@@ -23,6 +23,25 @@ vector<int> nextSmallerElement(vector<int> &input)
     return ans;
 }
 
+vector<int> nextSmallerElementRight(vector<int> &input)
+{
+    int n = input.size();
+    vector<int> ans(n, -1);
+    stack<int> st;
+
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && input[st.top()] > input[i])
+        {
+            ans[st.top()] = input[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    return ans;
+}
+
 int main()
 {
 
@@ -33,11 +52,17 @@ int main()
     input.push_back(3);
 
     vector<int> ans = nextSmallerElement(input);
+    vector<int> ans1 = nextSmallerElementRight(input);
 
     cout << "Printing" << endl;
     for (int i = 0; i < input.size(); i++)
     {
         cout << ans[i] << " ";
+    }
+    
+    for (int i = 0; i < input.size(); i++)
+    {
+        cout << ans1[i] << " ";
     }
 
     return 0;
