86ed2283d473b1208d68bffe3402483bc7dace74
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-02-15 20:46:47 +0530

    Next Greater Element

---
+++ Stacks/Stacks Problems/GFG-Next_Greater_Element.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+// TC => O(n)
+// SC => O(n)
+
+class Solution
+{
+public:
+    vector<long long> nextLargerElement(vector<long long> &arr, int n)
+    {
+        vector<long long> ans(n, -1);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++)
+        {
+            while (!st.empty() && arr[st.top()] < arr[i])
+            {
+                ans[st.top()] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
