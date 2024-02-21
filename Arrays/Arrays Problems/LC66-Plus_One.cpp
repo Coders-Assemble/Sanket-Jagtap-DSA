ea2c3aa943cc4b60a0980cbdfd71a0c6b6dacc03
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-02-22 00:14:24 +0530

    Plus One

---
+++ Arrays/Arrays Problems/LC66-Plus_One.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// TC => O(n)
+// SC => O(1)
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int n = digits.size();
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (digits[i] != 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits.insert(digits.begin(), 1);
+
+        return digits;
+    }
+};
