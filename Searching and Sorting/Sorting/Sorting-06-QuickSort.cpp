8e508f73bea0c46156eb855e96b7936763b560c8
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-03-12 12:57:27 +0530

    Quick Sort approach 2

--- Searching and Sorting/Sorting/Sorting-06-QuickSort.cpp
+++ Searching and Sorting/Sorting/Sorting-06-QuickSort.cpp
@@ -53,3 +53,73 @@ public:
         return nums;
     }
 };
+
+// Approach 2
+
+int partition(int *arr, int s, int e)
+{
+    int pI = s;
+    int pivotElement = arr[s];
+
+    int count = 0;
+    for (int i = s + 1; i <= e; i++)
+    {
+        if (arr[i] <= pivotElement)
+            count++;
+    }
+
+    swap(arr[pI], arr[s + count]);
+    pI = s + count;
+
+    int i = s;
+    int j = e;
+
+    while (i < pI && j > pI)
+    {
+        while (arr[i] <= arr[pI])
+            i++;
+
+        while (arr[j] > arr[pI])
+            j--;
+
+        if (i < pI && j > pI)
+        {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+    return pI;
+}
+
+void quickSort(int *arr, int s, int e)
+{
+    if (s >= e)
+        return;
+
+    // Partitioning logic
+    int pI = partition(arr, s, e);
+
+    // Recursive call for left part
+    quickSort(arr, s, pI - 1);
+
+    // Recursive call for right part
+    quickSort(arr, pI + 1, e);
+}
+
+int main()
+{
+    int input[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    int n = 11;
+
+    quickSort(input, 0, n - 1);
+
+    cout << "Printing the sorted array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << input[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
