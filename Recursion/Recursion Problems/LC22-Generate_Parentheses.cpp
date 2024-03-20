1bb99471d755cd92752e411090d4a7e41aa3f529
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-03-20 20:49:25 +0530

    Generate Parentheses

--- Recursion/Recursion Problems/LC22-Generate_Parentheses.cpp
+++ Recursion/Recursion Problems/LC22-Generate_Parentheses.cpp
@@ -5,6 +5,45 @@ using namespace std;
 class Solution
 {
 public:
+    void solve(int open, int close, vector<string> &ans, string output)
+    {
+        if (open == 0 && close == 0)
+        {
+            ans.push_back(output);
+            return;
+        }
+
+        if (open > 0)
+        {
+            output.push_back('(');
+            solve(open - 1, close, ans, output);
+            // Backtracking
+            output.pop_back();
+        }
+
+        if (close > open)
+        {
+            output.push_back(')');
+            solve(open, close - 1, ans, output);
+            // Backtracking
+            output.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n)
+    {
+        vector<string> ans;
+        string output = "";
+        int open = n;
+        int close = n;
+        solve(open, close, ans, output);
+        return ans;
+    }
+};
+
+class Solution
+{
+public:
     vector<string> generateParenthesis(int n)
     {
         vector<string> v;
