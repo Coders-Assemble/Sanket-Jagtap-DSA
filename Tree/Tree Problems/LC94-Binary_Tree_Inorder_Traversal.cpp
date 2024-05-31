45adadeeb969bb8648d11c065e71fb78bc4485ca
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-05-31 21:06:26 +0530

    Morris Inorder Traversal

--- Tree/Tree Problems/LC94-Binary_Tree_Inorder_Traversal.cpp
+++ Tree/Tree Problems/LC94-Binary_Tree_Inorder_Traversal.cpp
@@ -93,3 +93,47 @@ public:
         return ans;
     }
 };
+
+// Morris Inorder Traversal
+// TC => O(n)
+// SC => O(1)
+
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        TreeNode *curr = root;
+
+        while (curr != NULL)
+        {
+            if (curr->left == NULL)
+            {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else
+            {
+                TreeNode *pred = curr->left;
+                while (pred->right != curr && pred->right != NULL)
+                {
+                    pred = pred->right;
+                }
+
+                if (pred->right == NULL)
+                {
+                    pred->right = curr;
+                    curr = curr->left;
+                }
+                else
+                {
+                    pred->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
